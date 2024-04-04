cd69df44b062e8e89a5e8876a907278d6dcf7fda
Shylet Manuel <manuelshylet@gmail.com>
2024-04-04 12:22:09 +0200

    added media file model

--- archiveapp/models.py
+++ archiveapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class MediaFile(models.Model):
+    FILE_TYPE_CHOICES = (
+        ('video', 'Video'),
+        ('image', 'Image'),
+        ('document', 'Document'),
+        ('audio', 'Audio'),
+    )
+
+    file_type = models.CharField(max_length=10, choices=FILE_TYPE_CHOICES, default='image')
+    title = models.CharField(max_length=255)
+    description = models.TextField(default='')
+    file = models.FileField(upload_to='media/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
