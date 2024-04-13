51761aea34f9539bfd3319bf40450902979eff16
Shylet Manuel <manuelshylet@gmail.com>
2024-04-13 07:26:17 +0200

    updated myFunc and added search func

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,64 @@
-// toggle btn
-function myFunc(){
+function myFunc() {
     var navbar = document.getElementById('nav');
     navbar.classList.toggle('show');
+    
+    // Check if nav is visible
+    if(navbar.classList.contains('show')) {
+        //to add event listener to close nav when clicking outside
+        document.addEventListener('click', closeNavOnClickOutside);
+    } else {
+        // to remove event listener to close nav when clicking outside
+        document.removeEventListener('click', closeNavOnClickOutside);
+    }
 }
+
+function closeNavOnClickOutside(event) {
+    var navbar = document.getElementById('nav');
+    var toggleBtn = document.querySelector('.toggle-btn');
+
+    if (!navbar.contains(event.target) && !toggleBtn.contains(event.target)) {
+        navbar.classList.remove('show');
+        document.removeEventListener('click', closeNavOnClickOutside);
+    }
+}
+$(document).ready(function(){
+    $('#search-form').on('submit', function(event){
+        event.preventDefault();
+        var query = $(this).find('input[name="q"]').val();
+        $.ajax({
+            url: $(this).attr('action'),
+            type: 'GET',
+            data: { 'q': query },
+            success: function(response){
+                $('#search-results').html(response);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    });
+});
+$(document).ready(function(){
+    console.log("Autocomplete script loaded");  
+
+    $("#search-input").autocomplete({
+        source: function(request, response) {
+            console.log("Autocomplete request for:", request.term);  
+            
+            $.ajax({
+                url: "{% url 'autocomplete_search' %}",
+                type: 'GET',
+                data: { 'q': request.term },
+                dataType: 'json',
+                success: function(data) {
+                    console.log("Received suggestions:", data.suggestions);  
+                    response(data.suggestions);
+                },
+                error: function(error){
+                    console.log("Error:", error);
+                }
+            });
+        },
+        minLength: 2
+    });
+});
