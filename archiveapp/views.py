595aa7e7b31807140b74ee6b9f4afce58c2b2563
Shylet Manuel <manuelshylet@gmail.com>
2024-04-13 07:20:02 +0200

    added search ,delete and edit media functionalities

--- archiveapp/views.py
+++ archiveapp/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.files.storage import default_storage
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
+from django.db.models import Q
 from django.contrib import messages
 from .models import MediaFile
-from .forms import MediaFileForm
+from .forms import MediaFileForm, MediaFileEditForm
+from django.http import JsonResponse
 
 
 @login_required(login_url='login_user')
@@ -72,6 +75,62 @@ def upload(request):
     else:
         form = MediaFileForm()
     return render(request, 'upload.html', {'form': form})
+
+@login_required(login_url='login')
+def edit_media(request, media_id):
+    try:
+        media_item = MediaFile.objects.get(pk=media_id)
+        
+        if request.method == 'POST':
+            form = MediaFileForm(request.POST, request.FILES, instance=media_item)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Media file updated successfully!')
+                return redirect('home')
+        else:
+            form = MediaFileForm(instance=media_item)
+        
+        return render(request, 'edit_media.html', {'form': form, 'media_item': media_item})
+    
+    except ObjectDoesNotExist:
+        messages.error(request, 'Media file not found.')
+        return redirect('upload')
+    except Exception as e:
+        messages.error(request, f'An error occurred: {e}')
+        return redirect('upload')
+
+@login_required(login_url='login')
+def delete_media(request, media_id):
+    try:
+        media_item = MediaFile.objects.get(pk=media_id)
+        media_item.delete()
+        messages.success(request, 'Media file deleted successfully!')
+    except MediaFile.DoesNotExist:
+        messages.error(request, 'Media file not found.')
+    except Exception as e:
+        messages.error(request, f'An error occurred: {e}')
+
+    return redirect('home')
+
+def search_media(request):
+    query = request.GET.get('q')
+    if query:
+        media = MediaFile.objects.filter(Q(title__icontains=query) | Q(description__icontains=query)).order_by('-uploaded_at')
+    else:
+        media = MediaFile.objects.none()
+    return render(request, 'search_media.html', {'media': media, 'query': query})
+
+def autocomplete_search(request):
+    query = request.GET.get('q')
+    suggestions = []
+    if query:
+        media = MediaFile.objects.filter(
+            Q(file_name__icontains=query) | 
+            Q(description__icontains=query) |
+            Q(file_type__icontains=query)
+        ).order_by('-uploaded_at').values_list('title', flat=True)[:10]  # Limit to 10 suggestions
+        suggestions = list(media)
+    return JsonResponse({'suggestions': suggestions})
 
 def audio(request):
     media = MediaFile.objects.filter(file_type='audio').order_by('-uploaded_at')
