0e0e42a3cb88bc9b00b2d20087d4bce2835e916d
Shylet Manuel <manuelshylet@gmail.com>
2024-04-13 07:20:44 +0200

    add edit media form

--- archiveapp/forms.py
+++ archiveapp/forms.py
@@ -5,3 +5,8 @@ class MediaFileForm(forms.ModelForm):
     class Meta:
         model = MediaFile
         fields = [ 'title', 'description', 'file_type','file']
+
+class MediaFileEditForm(forms.ModelForm):
+    class Meta:
+        model = MediaFile
+        fields = ['file_type', 'title', 'description']
